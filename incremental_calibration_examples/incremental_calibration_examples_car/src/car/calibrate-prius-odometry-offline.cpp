818240dff581f8f9d4d8a952b8bf0ccbb8bc9528
Jerome Maye <jerome.maye@mavt.ethz.ch>
2013-05-05 13:07:22 +0000

    * nearly ready for offline optimization of odometry in PRIUS

--- incremental_calibration_examples/incremental_calibration_examples_car/src/car/calibrate-prius-odometry-offline.cpp
+++ incremental_calibration_examples/incremental_calibration_examples_car/src/car/calibrate-prius-odometry-offline.cpp
@@ -24,10 +24,147 @@
 #include <iostream>
 #include <fstream>
 
+#include <Eigen/Core>
+
+#include <boost/shared_ptr.hpp>
+
+#include <sm/kinematics/RotationVector.hpp>
+#include <sm/kinematics/EulerAnglesYawPitchRoll.hpp>
+
+#include <aslam/backend/OptimizationProblem.hpp>
+#include <aslam/backend/Optimizer2Options.hpp>
+#include <aslam/backend/SparseQrLinearSystemSolver.hpp>
+#include <aslam/backend/SparseQRLinearSolverOptions.h>
+#include <aslam/backend/Optimizer2.hpp>
+
+#include <bsplines/BSplinePose.hpp>
+
+#include <aslam/splines/BSplinePoseDesignVariable.hpp>
+
+#include "aslam/calibration/car/CANBinaryParser.h"
+#include "aslam/calibration/car/ApplanixBinaryParser.h"
+#include "aslam/calibration/car/utils.h"
+#include "aslam/calibration/car/ErrorTermPose.h"
+
+using namespace aslam::calibration;
+using namespace sm::kinematics;
+using namespace bsplines;
+using namespace aslam::splines;
+using namespace aslam::backend;
+
 int main(int argc, char** argv) {
-  if (argc != 2) {
-    std::cerr << "Usage: " << argv[0] << " <LogFilename>" << std::endl;
+  if (argc != 3) {
+    std::cerr << "Usage: " << argv[0]
+      << " <applanix-binary-log> <can-binary-log>" << std::endl;
     return -1;
   }
+
+  // parse the Applanix log file
+  std::cout << "Parsing Applanix data..." << std::endl;
+  ApplanixBinaryParser applanixParser(argv[1]);
+  applanixParser.parse();
+
+  // parse the CAN log file
+  std::cout << "Parsing CAN data..." << std::endl;
+  CANBinaryParser canParser(argv[2]);
+  canParser.parse();
+
+  // create rotation parameterizations
+  boost::shared_ptr<RotationVector> rv(new RotationVector);
+  const EulerAnglesYawPitchRoll ypr;
+
+  // create B-Spline
+  const int order = 4;
+  BSplinePose bspline(order, rv);
+
+  // iterate over the parsed Applanix messages
+  std::cout << "Creating data for B-spline..." << std::endl;
+  const size_t numMeasurements = applanixParser.getNumNavigationSolution();
+  Eigen::VectorXd timestamps(numMeasurements);
+  Eigen::Matrix<double, 6, Eigen::Dynamic> poses(6, numMeasurements);
+  size_t i = 0;
+  for (auto it = applanixParser.cbegin(); it != applanixParser.cend(); ++it) {
+    Eigen::Vector3d crv = rv->rotationMatrixToParameters(
+      ypr.parametersToRotationMatrix(Eigen::Vector3d(it->second.yaw,
+      it->second.pitch, it->second.roll)));
+    // ensure rotation vector do not flip
+    if (i > 0) {
+      Eigen::Matrix<double, 6, 1> lastPose = poses.col(i - 1);
+      crv = rotVectorNoFlipping(lastPose.tail<3>(), crv);
+    }
+    timestamps(i) = it->first;
+    Eigen::Matrix<double, 6, 1> pose;
+    pose << it->second.x, it->second.y, it->second.z, crv;
+    poses.col(i) = pose;
+    ++i;
+  }
+
+  std::cout << "Number of measurements: " << numMeasurements << std::endl;
+  const double elapsedTime =
+    timestamps(numMeasurements - 1) - timestamps(0);
+  std::cout << "Sequence length [s]: " << elapsedTime << std::endl;
+
+  // fill the B-Spline with measurements
+  const double lambda = 1e-6;
+  const int measPerSec = numMeasurements / elapsedTime;
+  const int measPerSecDesired = 5;
+  int numSegments;
+  if (measPerSec > measPerSecDesired)
+    numSegments = measPerSecDesired * elapsedTime;
+  else
+    numSegments = numMeasurements;
+  std::cout << "Creating B-Spline with " << numSegments << " segments..."
+    << std::endl;
+  bspline.initPoseSplineSparse(timestamps, poses, numSegments, lambda);
+
+  // create optimization problem
+  boost::shared_ptr<aslam::backend::OptimizationProblem> problem(
+    new aslam::backend::OptimizationProblem);
+
+  // create B-spline design variables
+  std::cout << "Creating B-spline design variables..." << std::endl;
+  boost::shared_ptr<BSplinePoseDesignVariable> bspdv(
+    new BSplinePoseDesignVariable(bspline));
+  for (size_t i = 0; i < bspdv->numDesignVariables(); ++i) {
+    bspdv->designVariable(i)->setActive(true);
+    problem->addDesignVariable(bspdv->designVariable(i), false);
+  }
+
+  // create error terms for each Applanix measurement
+  std::cout << "Creating error terms for Applanix measurements..." << std::endl;
+  i = 0;
+  for (auto it = applanixParser.cbegin(); it != applanixParser.cend(); ++it) {
+    ErrorTermPose::Input xm;
+    xm.head<3>() = Eigen::Vector3d(it->second.x, it->second.y, it->second.z);
+    xm.tail<3>() = Eigen::Vector3d(it->second.yaw, it->second.pitch,
+      it->second.roll);
+    aslam::calibration::ErrorTermPose::Covariance Q =
+      aslam::calibration::ErrorTermPose::Covariance::Zero();
+    Q(0, 0) = it->second.x_sigma2;
+    Q(1, 1) = it->second.y_sigma2;
+    Q(2, 2) = it->second.z_sigma2;
+    Q(3, 3) = it->second.yaw_sigma2;
+    Q(4, 4) = it->second.pitch_sigma2;
+    Q(5, 5) = it->second.roll_sigma2;
+    boost::shared_ptr<ErrorTermPose> e_pose(new ErrorTermPose(
+      bspdv->transformation(timestamps(i)), xm, Q));
+    problem->addErrorTerm(e_pose);
+    ++i;
+  }
+
+  // optimize
+  std::cout << "Optimizing..." << std::endl;
+  aslam::backend::Optimizer2Options options;
+  options.verbose = true;
+  options.doLevenbergMarquardt = false;
+  options.linearSolver = "sparse_qr";
+  SparseQRLinearSolverOptions linearSolverOptions;
+  linearSolverOptions.colNorm = true;
+  Optimizer2 optimizer(options);
+  optimizer.getSolver<SparseQrLinearSystemSolver>()->setOptions(
+    linearSolverOptions);
+  optimizer.setProblem(problem);
+  optimizer.optimize();
+
   return 0;
 }
